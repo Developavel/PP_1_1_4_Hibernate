9752f5d98987f76922f1cac049c325a262879606
Developavel <pa.mordovin@gmail.com>
2026-04-10 00:22:33 +0300

    Update Main.java and Util.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -11,24 +11,29 @@ public class Main { // Запуск и демонстрация
 
         UserService userService = new UserServiceImpl();
 
-        userService.createUsersTable();
-        System.out.println("Таблица users создана\n");
+        userService.createUsersTable(); // создание таблицы
+        System.out.println("Таблица users создана");
+        System.out.println("=".repeat(70)); // разделитель
 
-        userService.saveUser("Иван", "Иваныч", (byte) 25);
-        userService.saveUser("Петр", "Петров", (byte) 30);
-        userService.saveUser("Мария", "Сидорова", (byte) 28);
-        userService.saveUser("Анна", "Кузнецова", (byte) 22);
+        userService.saveUser("Иван", "Иваныч", (byte) 23); // добавление пользователя
+        userService.saveUser("Петр", "Петров", (byte) 30); // добавление пользователя
+        userService.saveUser("Мария", "Сидорова", (byte) 27); // добавление пользователя
+        userService.saveUser("Анна", "Кузнецова", (byte) 37); // добавление пользователя
+        System.out.println("=".repeat(70)); // разделитель
 
-        System.out.println("\nСписок всех пользователей таблицы users: ");
-        List<User> users = userService.getAllUsers();
+        System.out.println("Список всех пользователей таблицы users: ");
+        List<User> users = userService.getAllUsers(); // получение всех пользователей
         for (User user : users) {
             System.out.println(user);
         }
+        System.out.println("=".repeat(70)); // разделитель
 
-        userService.cleanUsersTable();
-        System.out.println("\nТаблица users очищена\n");
+        userService.cleanUsersTable(); // очистка таблицы
+        System.out.println("Таблица users очищена");
+        System.out.println("=".repeat(70)); // разделитель
 
-        userService.dropUsersTable();
-        System.out.println("Таблица users удалена\n");
+        userService.dropUsersTable(); // удаление таблицы
+        System.out.println("Таблица users удалена");
+        System.out.println("=".repeat(70)); // разделитель
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -2,13 +2,14 @@ package jm.task.core.jdbc.util;
 
 import jm.task.core.jdbc.model.User;
 
-import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+
 
 public class Util { // класс для подключения к БД (JDBC и Hibernate)
 
@@ -28,30 +29,19 @@ public class Util { // класс для подключения к БД (JDBC и
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
 
-    private static SessionFactory sessionFactory; // фабрика, которая создает сессии для работы с БД
+    private static SessionFactory sessionFactory; // создает сессии Hibernate для работы с БД
 
-    public static SessionFactory getSessionFactory() {
+    public static SessionFactory getSessionFactory() { // конфиг Hibernate
         if (sessionFactory == null) {
             try {
-                Configuration configuration = new Configuration();
-
-                // настройка подключения Hibernate к БД
-                configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
-                configuration.setProperty("hibernate.connection.url", URL);
-                configuration.setProperty("hibernate.connection.username", USERNAME);
-                configuration.setProperty("hibernate.connection.password", PASSWORD);
-
-                // настройки работы Hibernate
-                configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect"); // указываем диалект MySQL 8
-                configuration.setProperty("hibernate.show_sql", "true"); // показывать SQL-запросы в консоли
-                configuration.setProperty("hibernate.format_sql", "true"); // форматировать SQL для читаемости
-                configuration.setProperty("hibernate.hbm2ddl.auto", "update"); // автоматически обновлять схему БД
-
-                // регистрация сущности User, которую Hibernate будет отслеживать
-                configuration.addAnnotatedClass(User.class);
-
-                sessionFactory = configuration.buildSessionFactory(); // создаем фабрику сессий для работы с БД
-            } catch (HibernateException e) {
+                sessionFactory = new Configuration()
+                        .setProperty("hibernate.connection.url", URL)
+                        .setProperty("hibernate.connection.username", USERNAME)
+                        .setProperty("hibernate.connection.password", PASSWORD)
+                        .setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver")
+                        .addAnnotatedClass(User.class)
+                        .buildSessionFactory();
+            } catch (Throwable e) {
                 throw new RuntimeException("Ошибка создания SessionFactory", e);
             }
         }
